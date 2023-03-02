10fbea20a4737a8b715151646b875a3d8022d777
Braeden Lhamon <S543387@nwmissouri.edu>
2023-03-02 08:37:33 -0600

    Creating the Intents for TrophyPage
    Beginning to formulate the intents needed to go from home page (Main Activity) to TrophyPage

--- app/src/main/java/com/example/puzzlebites/MainActivity.java
+++ app/src/main/java/com/example/puzzlebites/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.puzzlebites;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityResultLauncher<Intent> trophyPageLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult trophyPageExit) {
+                        /*My plan is to use one ActivityResult for the exit button and
+                            the back space (returnBTN) button
+                          "Return" will be the returnBTN, else will be the exitBTN
+                        */
+                        int resultTrophy = trophyPageExit.getResultCode();
+                        Intent trophyData = trophyPageExit.getData();
+                        if(trophyData.getBooleanExtra("Return", false));
+                        /*int result = activityResultFence.getResultCode();
+                        Intent data = activityResultFence.getData();
+                        assert data != null;
+                        Global.area = data.getIntExtra("areaStr",0);
+                        TextView outputTV = findViewById(R.id.outputTV);
+                        float cost = (Global.area/(float)coverage*Global.costPerLiter);
+                        litersUsed = Global.area/(float)coverage*(float)3.8;
+                        int rounded = round(cost);
+                        //find 10 percent below and above
+                        int belowInt = (int) ((int) rounded*.90);
+                        int aboveInt = (int) ((int) rounded*1.10);
+                        outputTV.setText("Using " + String.format("%.2f",litersUsed) + " cans of paint, the cost will be anywhere from $" + belowInt
+                                + " to $" + aboveInt);*/
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/puzzlebites/TrophyPage.java
+++ app/src/main/java/com/example/puzzlebites/TrophyPage.java
@@ -1,8 +1,14 @@
 package com.example.puzzlebites;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TrophyPage extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class TrophyPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy_page2);
+
+        ActivityResultLauncher<Intent> trophyPageLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult trophyPageExit) {
+                        /*My plan is to use one ActivityResult for the exit button and
+                            the back space (returnBTN) button
+                          "Return" will be the returnBTN, else will be the exitBTN
+                        */
+                        int resultTrophy = trophyPageExit.getResultCode();
+                        Intent trophyData = trophyPageExit.getData();
+                        if(trophyData.getBooleanExtra("Return", false));
+                        /*int result = activityResultFence.getResultCode();
+                        Intent data = activityResultFence.getData();
+                        assert data != null;
+                        Global.area = data.getIntExtra("areaStr",0);
+                        TextView outputTV = findViewById(R.id.outputTV);
+                        float cost = (Global.area/(float)coverage*Global.costPerLiter);
+                        litersUsed = Global.area/(float)coverage*(float)3.8;
+                        int rounded = round(cost);
+                        //find 10 percent below and above
+                        int belowInt = (int) ((int) rounded*.90);
+                        int aboveInt = (int) ((int) rounded*1.10);
+                        outputTV.setText("Using " + String.format("%.2f",litersUsed) + " cans of paint, the cost will be anywhere from $" + belowInt
+                                + " to $" + aboveInt);*/
+                    }
+                }
+        );
     }
 }
