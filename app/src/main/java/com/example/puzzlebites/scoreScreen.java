3789e5fd4125357136c15fd5b76319bc6206ae10
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2023-03-02 21:32:47 -0600

    Implemented the Retry Screen

---
+++ app/src/main/java/com/example/puzzlebites/scoreScreen.java
@@ -0,0 +1,18 @@
+package com.example.puzzlebites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scoreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_screen);
+    }
+}
+
+
+
+        // Code will be put here to handle where to go
