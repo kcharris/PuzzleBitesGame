53da6384d1f2872374d447db3d6bc63babdebcab
Braeden Lhamon <S543387@nwmissouri.edu>
2023-03-03 22:41:25 -0600

    Braeden - creating functionality for returnFromHomeBTN, retryBTN, and returnHomeBTN
    Updated the scoreScreen.java file to add functionality to returnFromGameBTN, returnHomeBTN, and retryBTN

--- app/src/main/java/com/example/puzzlebites/scoreScreen.java
+++ app/src/main/java/com/example/puzzlebites/scoreScreen.java
@@ -1,8 +1,15 @@
 package com.example.puzzlebites;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class scoreScreen extends AppCompatActivity {
 
@@ -10,6 +17,38 @@ public class scoreScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_screen);
+        mpStartLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK)
+                        {
+
+                        }
+                    }
+                });
+    }
+
+
+    private ActivityResultLauncher<Intent> mpStartLauncher;
+
+    public void returnBTN(View v) {
+        Intent mainPageIntent = new Intent(this, MainPageActivity.class);
+        mainPageIntent.putExtra("Return", 0);
+        setResult(Activity.RESULT_OK, mainPageIntent);
+        finish();
+    }
+
+    public void exitBTN(View v) {
+        Intent mainPageIntent = new Intent(this, MainPageActivity.class);
+        setResult(Activity.RESULT_OK, mainPageIntent);
+        finish();
+    }
+
+    public void retryBTN(View v) {
+        Intent retryPuzzle = new Intent(this, PuzzleActivity.class);
+
     }
 }
 
