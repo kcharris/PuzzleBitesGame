f11e01a3e3df0478a65c2a56c69b41728f3c9e13
kevin harris <kcharris77@gmail.com>
2023-03-03 22:20:14 -0600

    Add back button onclick to puzzleActivity

--- app/src/main/java/com/example/puzzlebites/PuzzleActivity.java
+++ app/src/main/java/com/example/puzzlebites/PuzzleActivity.java
@@ -2,7 +2,10 @@ package com.example.puzzlebites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PuzzleActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class PuzzleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+    }
+    public void returnMain(View v)
+    {
+        Intent mainIntent = new Intent(this, MainPageActivity.class);
+        mainIntent.putExtra("Return", 0);
+        setResult(Activity.RESULT_OK, mainIntent);
+        finish();
     }
 }
