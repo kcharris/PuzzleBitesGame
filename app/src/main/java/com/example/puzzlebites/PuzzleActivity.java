2a917f0ce656e064d7e3be23cb55180e07638e0c
Braeden Lhamon <S543387@nwmissouri.edu>
2023-03-03 22:24:52 -0600

    Braeden - Implemented Skip Puzzle button on PuzzleActivity
    Created SkipPuzzle Method for the PuzzleActivity

--- app/src/main/java/com/example/puzzlebites/PuzzleActivity.java
+++ app/src/main/java/com/example/puzzlebites/PuzzleActivity.java
@@ -1,8 +1,15 @@
 package com.example.puzzlebites;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PuzzleActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class PuzzleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+
+        sStartLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK)
+                        {
+
+                        }
+                    }
+                });
+    }
+
+
+    private ActivityResultLauncher<Intent> sStartLauncher;
+
+    public void skipPuzzle(View v) {
+        Intent puzzle = new Intent(this,PuzzleActivity.class);
+        puzzle.putExtra("puzzle",0);
+        setResult(Activity.RESULT_OK, puzzle);
+        sStartLauncher.launch(puzzle);
     }
 }
