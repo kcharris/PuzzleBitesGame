01b4e8fee87f7d77a9ac59b4d7998307e2d5b0d8
Braeden Lhamon <S543387@nwmissouri.edu>
2023-04-12 21:24:09 -0500

    Braeden - Completed assignment of tasks and laid out a potential system for scores
    I finished a backlog task that was about creating other tasks in relation to the settings page. I also implemented a concept for the reset score method inside the settingsPage for how scores should work

--- app/src/main/java/com/example/puzzlebites/setting.java
+++ app/src/main/java/com/example/puzzlebites/setting.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +19,12 @@ public class setting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
     }
-
+    /*
+    Braeden - One of my tasks is to implement a reset button that's on the settings page. I think that
+    it would be wise to have a ModelClass that holds the scores for the instance of the activity running at the time
+    this would mean that settings should call a reset method from the implied Model
+    this would be the equivalent to a Global Variable but this shows understanding of class material
+    */
     private String backColor = "default";
     private String filename = "savedData";
     private String fileContent = backColor;
@@ -36,7 +42,10 @@ public class setting extends AppCompatActivity {
         backColor = "blue";
     }
 
-
+    public void resetScore() {
+        //model.reset(); this is just an example of my idea, awaiting implementation
+        //Luke might want to do this but I can if anyone or Luke would rather
+    }
 
     @Override
     protected void onStop()
