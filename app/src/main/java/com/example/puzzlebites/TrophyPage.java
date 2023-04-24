14eb3f5d64a00ddf9e300d8d6118a551daa11cb9
kevin harris <kcharris77@gmail.com>
2023-04-23 21:35:12 -0500

    Add star and score updates to trophy page
    Uses cat as a placeholder for 0 stars

--- app/src/main/java/com/example/puzzlebites/TrophyPage.java
+++ app/src/main/java/com/example/puzzlebites/TrophyPage.java
@@ -10,6 +10,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.puzzlebites.data.model.Setting;
@@ -37,9 +38,78 @@ public class TrophyPage extends AppCompatActivity {
                     }
                 }
         );
-        TextView numOfStarsTV = findViewById(R.id.textView);
+        setTextViews();
+    }
+    public void setStarImage(ImageView starIV, int starsEarned){
+        if(starsEarned == 1) {
+            starIV.setImageResource(R.drawable.bronzestar);
+        }
+        else if (starsEarned == 2){
+            starIV.setImageResource(R.drawable.silverstar);
+        }
+        else if(starsEarned == 3){
+            starIV.setImageResource(R.drawable.goldstar);
+        }
+        else{
+            starIV.setImageResource(R.drawable.catend);
+        }
+    }
+    public void setScoreButtonText(TextView btn,String level, int score){
+        if(score == 0){
+            btn.setText("Level " + level + ": not completed");
+        }
+        else{
+            btn.setText("Level " + level + ": completed in " + score + " moves.");
+        }
+    }
+
+    public void setTextViews(){
+        int stars = 0;
+        String level;
+        int score;
+        TextView numOfStarsTV = findViewById(R.id.totalStarsTV);
         numOfStars = setting.getTotalStars();
-        numOfStarsTV.setText("You Have " + numOfStars + " Stars!");
+        numOfStarsTV.setText("You Have " + numOfStars + " Stars out of 15!");
+
+        ImageView level1StarIV = findViewById(R.id.level1StarIV);
+        TextView level1BTN = findViewById(R.id.lv1BTN);
+        level = "one";
+        stars = setting.getStars(level);
+        score = setting.getScore(level);
+        setStarImage(level1StarIV, stars);
+        setScoreButtonText(level1BTN, level, score);
+
+        ImageView level2StarIV = findViewById(R.id.level2StarIV);
+        TextView level2BTN = findViewById(R.id.lv2BTN);
+        level = "two";
+        stars = setting.getStars(level);
+        score = setting.getScore(level);
+        setStarImage(level2StarIV, stars);
+        setScoreButtonText(level2BTN, level, score);
+
+        ImageView level3StarIV = findViewById(R.id.level3StarIV);
+        TextView level3BTN = findViewById(R.id.lv3BTN);
+        level = "three";
+        stars = setting.getStars(level);
+        score = setting.getScore(level);
+        setStarImage(level3StarIV, stars);
+        setScoreButtonText(level3BTN,level, score);
+
+        ImageView level4StarIV = findViewById(R.id.level4StarIV);
+        TextView level4BTN = findViewById(R.id.lv4BTN);
+        level = "four";
+        stars = setting.getStars(level);
+        score = setting.getScore(level);
+        setStarImage(level4StarIV, stars);
+        setScoreButtonText(level4BTN,level, score);
+
+        ImageView level5StarIV = findViewById(R.id.level5StarIV);
+        TextView level5BTN = findViewById(R.id.lv5BTN);
+        level = "five";
+        stars = setting.getStars(level);
+        score = setting.getScore(level);
+        setStarImage(level5StarIV, stars);
+        setScoreButtonText(level5BTN,level, score);
     }
     public void applySettings(){
         Setting.applySettingToView(myLayout);
