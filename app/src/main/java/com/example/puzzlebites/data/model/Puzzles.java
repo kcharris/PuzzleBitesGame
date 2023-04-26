5971678dd996ad1074231984b1ed3a468c4e8009
danielgatschet <danielgatschet@gmail.com>
2023-04-26 17:47:08 -0500

    Dan Gatschet- Added the puzzle for Level 2

--- app/src/main/java/com/example/puzzlebites/data/model/Puzzles.java
+++ app/src/main/java/com/example/puzzlebites/data/model/Puzzles.java
@@ -51,25 +51,24 @@ public class Puzzles {
                 p.goldThres = 8;
                 break;
             case LEVEL2:
-                p.endPieces.add(new Piece(context, END, 1, 1));
-                p.endPieces.add(new Piece(context, END, 2, 3));
-                p.endPieces.add(new Piece(context, END, 1, 6));
-                p.endPieces.add(new Piece(context, END, 3, 7));
-                p.endPieces.add(new Piece(context, END, 2, 8));
+                p.endPieces.add(new Piece(context, END, 2, 1));
+                p.endPieces.add(new Piece(context, END, 1, 3));
+                p.endPieces.add(new Piece(context, END, 5, 5));
+                p.endPieces.add(new Piece(context, END, 5, 6));
 
-                p.switchPieces.add(new Piece(context, BEAR_SWITCH, 5, 1));
-                p.switchPieces.add(new Piece(context, CAT_SWITCH, 6, 3));
+                p.switchPieces.add(new Piece(context, BEAR_SWITCH, 7, 4));
+                p.switchPieces.add(new Piece(context, BEAR_SWITCH, 6, 3));
+                p.switchPieces.add(new Piece(context, SOUP_SWITCH, 7, 3));
 
-                p.pieces.add(new Piece(context, CAT, 3, 1));
-                p.pieces.add(new Piece(context, CAT, 4, 3));
-                p.pieces.add(new Piece(context, BEAR, 1, 4));
                 p.pieces.add(new Piece(context, BEAR, 3, 5));
-                p.pieces.add(new Piece(context, BEAR, 2, 6));
-                p.pieces.add(new Piece(context, BAGEL, 6, 4));
+                p.pieces.add(new Piece(context, BEAR, 4, 3));
+                p.pieces.add(new Piece(context, SOUP, 5, 2));
+                p.pieces.add(new Piece(context, SOUP, 5, 3));
+                p.pieces.add(new Piece(context, BAGEL, 4, 7));
 
-                p.bronzeThres = 17;
-                p.silverThres = 12;
-                p.goldThres = 8;
+                p.bronzeThres = 28;
+                p.silverThres = 20;
+                p.goldThres = 14;
                 break;
             case LEVEL3:
                 p.endPieces.add(new Piece(context, END, 1, 1));
