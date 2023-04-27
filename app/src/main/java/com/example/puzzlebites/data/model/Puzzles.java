56dbd044bbf8af72c3bbb7509e5997d26c3801b0
lukevaughn20 <78869404+lukevaughn20@users.noreply.github.com>
2023-04-26 22:07:59 -0500

    Luke - Finished Puzzle 3

--- app/src/main/java/com/example/puzzlebites/data/model/Puzzles.java
+++ app/src/main/java/com/example/puzzlebites/data/model/Puzzles.java
@@ -71,24 +71,24 @@ public class Puzzles {
                 p.goldThres = 14;
                 break;
             case LEVEL3:
-                p.endPieces.add(new Piece(context, END, 1, 1));
-                p.endPieces.add(new Piece(context, END, 3, 2));
-                p.endPieces.add(new Piece(context, END, 6, 1));
-                p.endPieces.add(new Piece(context, END, 7, 3));
-                p.endPieces.add(new Piece(context, END, 8, 2));
+                p.endPieces.add(new Piece(context, END, 0, 3));
+                p.endPieces.add(new Piece(context, END, 1, 4));
+                p.endPieces.add(new Piece(context, END, 1, 5));
+                p.endPieces.add(new Piece(context, END, 7, 2));
+                p.endPieces.add(new Piece(context, END, 8, 3));
 
-                p.switchPieces.add(new Piece(context, BEAR_SWITCH, 1, 5));
-                p.switchPieces.add(new Piece(context, CAT_SWITCH, 3, 6));
+                p.switchPieces.add(new Piece(context, BEAR_SWITCH, 8, 5));
+                p.switchPieces.add(new Piece(context, CAT_SWITCH, 5, 3));
 
-                p.pieces.add(new Piece(context, CAT, 1, 3));
-                p.pieces.add(new Piece(context, CAT, 3, 4));
-                p.pieces.add(new Piece(context, BEAR, 4, 1));
-                p.pieces.add(new Piece(context, BEAR, 5, 3));
-                p.pieces.add(new Piece(context, BEAR, 6, 2));
-                p.pieces.add(new Piece(context, BAGEL, 4, 6));
+                p.pieces.add(new Piece(context, CAT, 7, 0));
+                p.pieces.add(new Piece(context, CAT, 8, 1));
+                p.pieces.add(new Piece(context, BEAR, 3, 4));
+                p.pieces.add(new Piece(context, BEAR, 4, 5));
+                p.pieces.add(new Piece(context, BEAR, 4, 6));
+                p.pieces.add(new Piece(context, BAGEL, 6, 5));
 
-                p.bronzeThres = 17;
-                p.silverThres = 12;
+                p.bronzeThres = 19;
+                p.silverThres = 10;
                 p.goldThres = 8;
                 break;
             case LEVEL4:
